63d868b861da58ce30b486577d8f40b6ff7f4a19
Stretzx <134774043+Stretzx@users.noreply.github.com>
2024-06-22 21:38:25 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+/* 
+ -- READ THIS --
+
+- You can change the server feature in index.html file and find the feature part / feature area
+- You can change your server about in index.html file and find the about part / about us area
+
+ -- CHANGE THE WEBSITE OPTION IN BELOW --
+*/
+let option = {
+    server_name: "LIMU-PS",
+    server_description: "Hai welcome to my website Iimu-ps",
+    owner: [
+        "Astrowezz",
+        "STRET"
+    ],
+    discord_invite_link: "https://discord.com/invite/cHJXGs3Fw9",
+    ip_address: "152.42.243.202",
+    host_for_android: "https://www.mediafire.com/file/xrcew7drqxnps6z/LimuPS.txt/file"
+}
+
